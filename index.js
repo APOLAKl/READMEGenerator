b7fcb6da4812ae9fd7bc5f61515f91ecd21768b8
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-03-21 00:13:07 -0300

    Fifth commit - added sample screenshots at assets folder

--- index.js
+++ index.js
@@ -1,77 +1,62 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // function to initialize app
 function init() {
   inquirer
-  .prompt([
-    // array of questions for user input
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is your GitHub username?',
-    },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'Give a Title to your Project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Give a simple description of your Project?',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Code to install required NPM?',
-      default: 'npm i inquirer@8.2.4',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Give a brief usage information of your Project?',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'How can users contribute to the Repo?',
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: 'Code to test your Project?',
-      default: 'npm test',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'What license does your project have?',
-      choices: [
-        "MIT",
-        "Apache2.0",
-        "Mozilla",
-        "GPL3.0",
-        "BSD3",
-      ]
-    },
-    
-  ])
-  .then((answers) => {
-    console.log(answers)
+    .prompt([
+      // array of questions for user input
+      {
+        type: "input",
+        name: "title",
+        message: "Give a Title to your Project?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Give a simple description of your Project?",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Code to install required NPM?",
+        default: "npm i inquirer@8.2.4",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Give a brief usage information of your Project?",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Code to test your Project?",
+        default: "npm test",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "Apache2.0", "Mozilla", "GPL3.0", "BSD3"],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
 
+      const badge = `![License: ${answers.license}](https://img.shields.io/badge/license-${answers.license}-green)`;
 
-    // const badge = `![License: ${answers.license}](https://img.shields.io/static/v1?label=license&message=${answers.license}&color=green)`;
-    const badge = `![License: ${answers.license}](https://img.shields.io/badge/license-${answers.license}-green)`;
-
-
-    const readmeText = `
+      const readmeText = `
 # < ${answers.title} >
 ${badge}
 
@@ -93,8 +78,6 @@ ${answers.description}
 
 * [Questions](#questions)
 
-* [Screenshots](#screenshots)
-
 
 ## Installation
 
@@ -108,11 +91,11 @@ ${answers.usage}
 
 ## License
 
-This project is licensed under ${answers.license} license.;
+This project is licensed under ${answers.license} license.
 
 ## Contributing
 
-${answers.contributing}
+If you want to contribute, feel free to email me at ${answers.email}.
 
 ## Tests
 
@@ -125,29 +108,20 @@ ${answers.test}
 
 ## Questions
 
-For questions, you may reach me at ${answers.email}. 
+For questions, bugs or issues, you may reach me at ${answers.email}. 
 You can check out my other work at ${answers.github}.
 
-## Screenshots
-
-![${answers.title} Screenshot](https://github.com/${answers.github}/??????????????.jpg)
-
-
-## 🔗Link to Deployed Page URL
-https://${answers.github}.github.io/????????????/
 
 ## 🔗 Link to GitHub Repo Page
-https://github.com/${answers.github}/??????????  [![${answers.github} Repo Page](https://img.shields.io/github/stars/${answers.github}/??????????style=social)](https://github.com/${answers.github}/????????????)
-    `
+https://github.com/${answers.github}/${answers.title}  [![${answers.github} Repo Page]
+`;
 
-    // function to write README file
-    fs.writeFile(`./${answers.title}.md`, readmeText, function() {
-      console.log("File written successfully!")
-    })
-
-  })
+      // function to write README file
+      fs.writeFile(`README.md`, readmeText, function () {
+        console.log("README File written successfully!");
+      });
+    });
 }
-
 
 // Function call to initialize app
 init();
